5f2f184562a6885ac7bdbf4445790cf2af794ee5
CarsonTitus <177804846+CarsonTitus@users.noreply.github.com>
2025-04-02 20:41:10 -0600

    Lab 3 Code

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -36,21 +36,19 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 One-Hot State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 1 0 0 0 0 0 0 0
+--|                  ON    | 0 0 0 0 0 0 0 1
+--|                  R1    | 0 0 0 0 0 0 1 0
+--|                  R2    | 0 0 0 0 0 1 0 0
+--|                  R3    | 0 0 0 0 1 0 0 0
+--|                  L1    | 0 0 0 1 0 0 0 0
+--|                  L2    | 0 0 1 0 0 0 0 0
+--|                  L3    | 0 1 0 0 0 0 0 0
 --|                 --------------------
---|
---|
 --+----------------------------------------------------------------------------
 --|
 --| REQUIRED FILES :
@@ -86,23 +84,54 @@ library ieee;
   use ieee.numeric_std.all;
  
 entity thunderbird_fsm is 
---  port(
-	
---  );
+    port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
 -- CONSTANTS ------------------------------------------------------------------
-  
+    signal f_Q      : std_logic_vector (7 downto 0) := "10000000";
+    signal f_Q_next : std_logic_vector (7 downto 0);    
+    
 begin
 
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
-	
+	-- LOGIC EQUATIONS
+	f_Q_next(0) <= f_Q(7) and i_left and i_right;
+	f_Q_next(1) <= (f_Q(7) and (not i_left) and i_right);
+	f_Q_next(2) <= f_Q(1);-- and f_Q(0);
+	f_Q_next(3) <= f_Q(2);
+	f_Q_next(4) <= f_Q(7) and i_left and (not i_right);
+	f_Q_next(5) <= f_Q(4);
+	f_Q_next(6) <= f_Q(5);
+	f_Q_next(7) <= (f_Q(7) and not i_left and not i_right) or f_Q(0) or f_Q(3) or f_Q(6);
     ---------------------------------------------------------------------------------
-	
-	-- PROCESSES --------------------------------------------------------------------
+    -- OUTPUT EQUATIONS
+    o_lights_L(0) <= f_Q(0) OR f_Q(4) OR f_Q(5) OR f_Q(6);
+    o_lights_L(1) <= f_Q(0) OR f_Q(5) OR f_Q(6);
+    o_lights_L(2) <= f_Q(0) OR f_Q(6);
     
+    o_lights_R(2) <= f_Q(0) OR f_Q(1) OR f_Q(2) OR f_Q(3);
+    o_lights_R(1) <= f_Q(0) OR f_Q(2) OR f_Q(3);
+    o_lights_R(0) <= f_Q(0) OR f_Q(3);
+    
+	-- PROCESSES --------------------------------------------------------------------
+	-- Register with Synchronous reset 
+register_proc : process (i_clk, i_reset)
+begin
+    if rising_edge(i_clk) then
+        if i_reset = '1' then
+            f_Q <= "10000000";
+        else
+            f_Q <= f_Q_next;
+        end if;
+    end if;
+end process register_proc;
 	-----------------------------------------------------					   
 				  
 end thunderbird_fsm_arch;
--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,111 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+	  port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
-	
+	-- Inputs
+	signal w_reset : std_logic := '0';
+	signal w_clk   : std_logic := '0';
+	signal w_left  : std_logic := '0';
+	signal w_right : std_logic := '0'; 
+	-- Outputs
+	signal w_lights_L: std_logic_vector(2 downto 0);
+	signal w_lights_R: std_logic_vector(2 downto 0);
 	-- constants
-	
+	constant k_clk_period : time := 10 ns;
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+   uut: thunderbird_fsm port map (
+          i_reset => w_reset,
+          i_clk => w_clk,
+          i_left => w_left,
+          i_right => w_right,
+          o_lights_L => w_lights_L,
+          o_lights_R => w_lights_R
+        );
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    	clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
-	
+	sim_proc : process
+	begin 
+	-- Test 1: making sure FSM starts in a known state (all off)
+	   w_reset <= '1';
+	   wait for k_clk_period * 2;
+	   w_reset <= '0';
+	   wait for k_clk_period * 2;
+	   assert w_lights_L = "000" and w_lights_R = "000"
+	       report "Startup failed, lights are on" severity failure;
+	       
+    -- Test 2: reset FSm to idle state (all off)
+	   w_left <= '0'; 
+	   w_right <= '0';
+	   wait for k_clk_period * 5;
+	   assert w_lights_L = "000" and w_lights_R = "000"
+	       report "Idle failed, lights still on" severity failure;
+	       
+    -- Test 3: Left turn signal 
+    -- Checks sequence of L0->L0,L1->L0,L1,L2->off(idle)
+	   w_left <= '1'; 
+	   w_right <= '0';
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "001" and w_lights_R = "000"
+	       report "Left turn step 1 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "011" and w_lights_R = "000"
+	       report "Left turn step 2 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "111" and w_lights_R = "000"
+	       report "Left turn step 3 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "000" and w_lights_R = "000"
+	       report "Return to Idle failed." severity failure;	      
+	        	   
+    -- Test 4: Right turn signal 
+    -- Checks sequence of R0->R0,R1->R0,R1,R2->off(idle)
+	   w_left <= '0'; 
+	   w_right <= '1';
+	   wait for k_clk_period * 1;	
+	   assert w_lights_L = "000" and w_lights_R = "001"
+	       report "Right turn step 1 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "000" and w_lights_R = "011"
+	       report "Right turn step 2 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "000" and w_lights_R = "111"
+	       report "Right turn step 3 failed." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "000" and w_lights_R = "000"
+	       report "Return to Idle failed." severity failure;	
+
+    -- Test 5: Hazard lights 
+	   w_left <= '1'; 
+	   w_right <= '1';
+	   wait for k_clk_period * 1;	
+	   assert w_lights_L = "111" and w_lights_R = "111"
+	       report "Hazard check failed, all lights not on." severity failure;
+	   wait for k_clk_period * 1;
+	   assert w_lights_L = "000" and w_lights_R = "000"
+	       report "Reset failed, lights still on." severity failure;
+	   wait;
+	end process;
 	-----------------------------------------------------	
-	
 end test_bench;
